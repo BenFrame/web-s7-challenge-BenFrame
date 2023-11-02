12be13de7886e7d33379573467fbe3a7d1f6d2d2
Ben Frame <brewerytattooer@gmail.com>
2023-11-02 15:59:04 -0400

    sprint 7 WIP

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
+import {Routes, Route, Link, useLocation} from 'react-router-dom'
 
 function App() {
+  const location = useLocation();
+  const currentRoute = location.pathname;
   return (
     <div id="app">
       <nav>
         {/* NavLinks here */}
+        <Link to= '/' class={ currentRoute === '/' ? 'active' : ''}>Home</Link>
+        <Link to='/order' class={ currentRoute === '/order' ? 'active' : ''}>Order</Link>
       </nav>
       {/* Route and Routes here */}
-      <Home />
-      <Form />
+    <Routes>
+      <Route path='/' element = {<Home />}/>
+      <Route path='/order' element = {<Form />}/>
+    </Routes>
     </div>
   )
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import * as yup from 'yup'
+import axios from 'axios'
 
 // 👇 Here are the validation errors you will use with Yup.
 const validationErrors = {
@@ -8,6 +10,22 @@ const validationErrors = {
 }
 
 // 👇 Here you will create your schema.
+const formSchema= yup.object().shape({
+  fullName: yup.string()
+  .min(3,validationErrors.fullNameTooShort)
+  .max(20,validationErrors.fullNameTooLong),
+  size: yup.string()
+  .oneOf(['small', 'medium', 'large'], 'size must be S or M or L'),
+  Pepperoni: yup.string(), 
+  'Green Peppers': yup.string(), 
+  Pineapple: yup.string(),
+  Mushrooms: yup.string(), 
+  Ham: yup.string()
+
+  
+})
+
+
 
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
@@ -17,45 +35,179 @@ const toppings = [
   { topping_id: '4', text: 'Mushrooms' },
   { topping_id: '5', text: 'Ham' },
 ]
+const initialFormValue = {
+  fullName: '', 
+  size: '', 
+  Pepperoni: false, 
+  'Green Peppers': false,
+  Pinapple: false, 
+  Mushrooms: false, 
+  Ham: false,
+}
+const initialFormErrors = {
+  fullName: '', 
+  size: '', 
+
+}
+
+
+
+
+
+
 
 export default function Form() {
+  const [ formValues, setFormValues ] = useState(initialFormValue)
+  const [ formErrors, setFormErrors ] = useState(initialFormErrors)
+  
+  const [submit, setSubmit] = useState(false)
+  const [success, setSuccess] = useState('')
+  const [failure, setFailure] = useState('')
+
+  useEffect(() => {
+    // console.log(formValues)
+    formSchema.isValid(formValues).then(setSubmit)
+    
+  }, [formValues])
+
+  const onChange = evt => {
+    let { type, name, checked, value } = evt.target
+    if (type == 'checkbox') value = checked
+    // console.log( name );
+    // console.log (value );
+    setFormValues({ ...formValues, [name]: value })
+    yup.reach(formSchema, name).validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+      .catch(err => {
+        // console.log(err)
+        // console.log(err.errors)
+        if ( err.errors ) {
+           setFormErrors({ ...formErrors, [name]: err.errors[0] })
+        }
+      })
+  }
+  
+ 
+  
+
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    // // axios.post(formValues)
+    // .then(res => {
+    //   console.log(res.data)
+    //   setSuccess(res.data.message)
+    //   setFailure(failure)
+    // }).catch(err => {
+    //   setFailure(err.response.data.message)
+    //   setSuccess(success)
+    
+    // })
+    let count = 0;
+    formValues.Pepperoni && count++;
+    formValues['Green Peppers' ] && count++;
+    formValues.Pineapple && count++;
+    formValues.Mushrooms && count++;
+    formValues.Ham && count++;
+
+
+
+  //  const successString = count ? 'My string with toppings' : 'my string without toppings'
+    
+    
+    let successString = '';
+    if ( count === 0 ) {
+      successString = 'no'
+    } else if ( count >= 1 ) {
+      successString = `${count}`
+    } 
+
+    
+
+    setSuccess(`Thank you for your order, ${formValues.fullName}! Your ${formValues.size} pizza with ${successString} toppings is on the way.`);
+    setFormValues(initialFormValue)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2>Order Your Pizza</h2>
-      {true && <div className='success'>Thank you for your order!</div>}
-      {true && <div className='failure'>Something went wrong</div>}
+      {success && <div className='success'>{success}</div>}
+      {failure && <div className='failure'>{failure}</div>}
 
       <div className="input-group">
         <div>
           <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" />
+          <input name = 'fullName' id = 'fullName' onChange={onChange} value={formValues.fullName} placeholder="Type full name"  type="text" />
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {formErrors.fullName && <div className='error'>{formErrors.fullName}</div>}
       </div>
 
       <div className="input-group">
         <div>
           <label htmlFor="size">Size</label><br />
-          <select id="size">
+          <select id="size" name= 'size' onChange={onChange} value={ formValues.size } >
             <option value="">----Choose Size----</option>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
             {/* Fill out the missing options */}
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {formErrors.size && <div className='error'>{formErrors.size}</div>}
       </div>
 
       <div className="input-group">
         {/* 👇 Maybe you could generate the checkboxes dynamically */}
-        <label key="1">
+        { toppings.map( (topping) => (
+          <label key={ topping.topping_id } name= 'toppings' onChange={onChange}>
+          <input
+            name={ topping.text }
+            type="checkbox"
+            checked={ formValues[topping.text] }
+            onChange={onChange}
+          />
+          {topping.text}<br />
+        </label>
+        ))}
+        {/* <label key="1">
           <input
             name="Pepperoni"
             type="checkbox"
           />
           Pepperoni<br />
         </label>
+        <label key="2">
+          <input
+            name="Green Peppers"
+            type="checkbox"
+          />
+          Green Peppers<br />
+        </label>
+        <label key="3">
+          <input
+            name="Pineapple"
+            type="checkbox"
+          />
+          Pineapple<br />
+        </label>
+        <label key="4">
+          <input
+            name="Mushrooms"
+            type="checkbox"
+          />
+          Mushrooms<br />
+        </label>
+        <label key="5">
+          <input
+            name="Ham"
+            type="checkbox"
+          />
+          Ham<br />
+        </label> */}
+        
       </div>
       {/* 👇 Make sure the submit stays disabled until the form validates! */}
-      <input type="submit" />
+      <input  type="submit" disabled={!submit} />
     </form>
   )
 }
